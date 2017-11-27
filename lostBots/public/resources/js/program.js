ab3a4752249c9331d02db95736bc810cbebd7155
Edward (Ted) Doyle <ted.doyle.phd@gmail.com>
2017-11-27 00:23:57 -0600

    Previou commit message sgould have read: Modified program.js to support counts on instructions.  Laying the ground so that for example Down three is the same as down, down, down. In addition to the minor chages.

--- lostBots/public/resources/js/program.js
+++ lostBots/public/resources/js/program.js
@@ -3,84 +3,95 @@
 /* representing a basic operation.  To implement counts on instructions (e.g. LEFT 6  */
 /* or loops and if statements the instruction will half to more than just an integer, */
 
-var instruction ={
-		UP: 0,    // Define the instructions in as integers a type in JavaScript
-		DOWN: 1,
-		LEFT: 2,
-		RIGHT: 3,
-		STOP: 4,
-		getDescrisption: function (opCode) {  // The method getDescription returns a String
-			                                  // representing the instruction
-			switch (opCode) {
-				case instruction.UP:
-					str = "UP";
-					break;
-				case instruction.DOWN:
-					str = "DOWN";
-					break;
-				case instruction.LEFT:
-					str = "LEFT";
-					break;
-				case instruction.RIGHT:
-					str = "RIGHT";
-					break;
-				case instruction.STOP:
-					str = "STOP";
-					break;
-				default:
-					str = "OTHER";
-				    break;
-			}
-			return str;
-		}		
+var Instruction = {
+    repetitionCount: 0,
+    UP: 0,    // Define the instructions in as integers a type in JavaScript
+	DOWN: 1,
+    LEFT: 2,
+	RIGHT: 3,
+	STOP: 4,
+    opCode: 0
 };
+
+function getDescrisption(instr) {  // The method getDescription returns a String
+    // representing the instruction
+    switch (instr.opCode) {
+        case Instruction.UP:
+            str = "UP";
+            break;
+        case Instruction.DOWN:
+            str = "DOWN";
+            break;
+        case Instruction.LEFT:
+            str = "LEFT";
+            break;
+        case Instruction.RIGHT:
+            str = "RIGHT";
+            break;
+        case Instruction.STOP:
+            str = "STOP";
+            break;
+        default:
+            str = "OTHER";
+            break;
+    }
+    return str + "&nbsp;" + instr.repetitionCount;
+}
+
+function Instruction(code, repCount) {
+    this.opCode = code;
+    this.repetitionCount = repCount;
+}
 
    //The program is an object (class) the stores the instruction in an array.
    // The important thing is they are stored in an ordered structure.
-var program = {
+function Program() {
+    programCounter = 0;
+    instructionList = new Array();
+}
 
-	
-	programCounter: 0,
-	instructionList: new Array(),
-	init: function () {                     // init() is a function that initializes (or resets) the variables in the object
+	function init (program) {                     // init() is a function that initializes (or resets) the variables in the object
 											// the variables in the program.  Note the it also erases the current program, if there is one.
-		instructionList = new Array();
-		programCounter = 0;
-	},
-	addInstruction: function (instr) {    // this function adds an instruction to the end of the program.
-		instructionList.push(instr);
-	},
-	execInstruction: function () {        // execInstruction, returns the next instruction, moving the program
+		program.instructionList = new Array();
+		program.programCounter = 0;
+	}
+
+	function addInstruction(program, instr) {    // this function adds an instruction to the end of the program.
+		program.instructionList.push(instr);
+	}
+
+	function execInstruction(program) {        // execInstruction, returns the next instruction, moving the program
 		                                  // counter to the next instruction.
-		if (programCounter >= instructionList.length) {  // If at the end of the program return the STOP instruction
-			return instruction.STOP;
+		if (program.programCounter >= program.instructionList.length) {  // If at the end of the program return the STOP instruction
+			return Instruction.STOP;
 		}
-		return instructionList[programCounter++];
-	},
-	restartProgram : function() {                // Sets the program counter back to zero so the program can be run again
-		programCounter = 0;
-	},
-	getProgramInstruction: function(location) {   //  return an instruction from the specified location
-		return instructionList[location];
-	},
-	getProgramCounter: function() {                // return the program conter
-		return programCounter;
-	},
-	setInstruction: function(location, instr) {     // chang an instruction at a specied plave in memory
-		instructionList[location] = instr;
-	},
-	deleteInstruction: function(location) {         // deleteInstruction, removes an instruction from memory
-		instructionList.slice(location, 1)
-	},
-	programSize : function() {                      // programSize, returns the number of instructions in the program
-		return instructionList.length;
-	},
-	setCurrInstruction: function(loc) {                // setCurrentInstruction sets the value of the program counter
-		if (loc >= 0 && loc < instructionList.length) {
-			programCounter = loc;
+		return program.instructionList[program.programCounter++];
+	}
+
+
+	function restartProgram(program) {                // Sets the program counter back to zero so the program can be run again
+		program.programCounter = 0;
+	}
+	function getProgramInstruction(program, location) {   //  return an instruction from the specified location
+		return program.instructionList[location];
+	}
+	function getProgramCounter(program) {                // return the program conter
+		return program.programCounter;
+	}
+	function setInstruction(program, location, opCode, repCount) {     // chang an instruction at a specied plave in memory
+        instr = new Instruction(opCode, repCount);
+		program.instructionList[location] = instr;
+	}
+
+	function deleteInstruction(program, location) {         // deleteInstruction, removes an instruction from memory
+		program.instructionList.slice(location, 1)
+	}
+
+	function programSize(program) {                      // programSize, returns the number of instructions in the program
+		return program.instructionList.length;
+	}
+	function setCurrInstruction(program, loc) {                // setCurrentInstruction sets the value of the program counter
+		if (loc >= 0 && loc < program.instructionList.length) {
+			program.programCounter = loc;
 		}
 	}
-};
-
-program.init();
-program.addInstruction(instruction.RIGHT);
